d8896e387eddd26f44553a6461b202a9576b754d
Michal Dydecki <michaldydecki@gmail.com>
2015-12-20 19:31:05 +0100

    update translations

--- searchform.php
+++ searchform.php
@@ -11,7 +11,9 @@
   	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'activello' ); ?></label>
     <input type="text" class="form-control search-query" placeholder="<?php echo esc_attr_x( 'Search&hellip;', 'placeholder', 'activello' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'activello' ); ?>" />
     <span class="input-group-btn">
-      <button type="submit" class="btn btn-default" name="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'activello' ); ?>">search</button>
+      <button type="submit" class="btn btn-default" name="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'activello' ); ?>">
+        <?php echo __('search', 'activello'); ?>
+      </button>
     </span>
   </div>
 </form>
