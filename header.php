d72b83e5327f18ad97d35a852eb3787544477b89
Michal Dydecki <michaldydecki@gmail.com>
2015-12-20 23:30:38 +0100

    added fb sdk

--- header.php
+++ header.php
@@ -28,6 +28,16 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v2.5&appId=535506156555789";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
 <div id="page" class="hfeed site">
 
 	<header id="masthead" class="site-header" role="banner">
